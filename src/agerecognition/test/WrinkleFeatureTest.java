94a323422b4eddd1f37526c1315a5da4c3414819
Mateusz <t.mateusz9333@gmail.com>
2018-05-23 02:04:29 +0200

    Srednio dzialajace klasyfikator nosa

--- src/agerecognition/test/WrinkleFeatureTest.java
+++ src/agerecognition/test/WrinkleFeatureTest.java
@@ -18,18 +18,36 @@ public class WrinkleFeatureTest {
     final String testImages = resourcesPath + "testImages/";
     final String trainingImages = resourcesPath + "trainingImages/";
     final String lenaPath = testImages + "lena.png";
+    final String facetPath = testImages + "facet.jpg";
+
+    private Mat croppedFace = null;
 
     static{
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
-    @org.junit.Test
-    public void faceDetector() throws InterruptedException {
+    @Test
+    public void faceDetectorTest() throws InterruptedException {
         showImage(lenaPath, "Oryginal");
-        Mat croppedFace  = WrinkleFeature.faceDetector(lenaPath);
+        croppedFace = WrinkleFeature.faceDetector(Imgcodecs.imread(lenaPath));
         Imshow.show(croppedFace, "Cropped face");
-        Thread.sleep(2000);
+        Thread.sleep(20000);
     }
+    @Test
+    public void detectEyesMouthNoseTest() throws InterruptedException {
+        croppedFace = WrinkleFeature.faceDetector(Imgcodecs.imread(facetPath));
+        Mat eyesMouthNose = WrinkleFeature.detectEyesMouthNose(croppedFace);
+        Imshow.show(eyesMouthNose, "Detected features");
+        Thread.sleep(20000);
+    }
+    @Test
+    public void detectEdgesTest() throws InterruptedException {
+
+        Mat edges = WrinkleFeature.detectEdges(Imgcodecs.imread(lenaPath));
+        Imshow.show(edges);
+        Thread.sleep(10000);
+    }
+
     void showImage(String imagePath, String frameTitle){
         Mat image = Imgcodecs.imread(imagePath);
         Imshow.show(image,frameTitle);
