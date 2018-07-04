a9967dd454e25238f8db8456039145daab48d8cb
Mateusz <t.mateusz9333@gmail.com>
2018-07-04 20:40:30 +0200

    Refaktor, rozwijanie klasyfikatora

---
+++ src/agerecognition/pl/mateusz/agerecognition/train/AgeClassifierTest.java
@@ -0,0 +1,44 @@
+package pl.mateusz.agerecognition.train;
+
+import org.junit.Test;
+import pl.mateusz.agerecognition.utils.Paths;
+
+import java.io.File;
+
+public class AgeClassifierTest {
+    @Test
+    public void generateDataFromImagesManual() {
+        AgeClassifier.generateDataFromImages("1", "41");
+    }
+
+    @Test
+    public void generateDataFromImagesAuto() throws InterruptedException {
+        boolean process = false;
+        File images = new File(Paths.trainingImagesPath);
+        String startFrom = "19";
+        for (File image : images.listFiles()) {
+
+            if (image.getName().contains(startFrom)) {
+                process = true;
+            }
+
+            if (process == false) {
+                continue;
+            }
+
+            AgeClassifier.generateDataFromImages("1", image.getName());
+            Thread.sleep(10000);
+        }
+    }
+
+    @Test
+    public void mergeAgeToWrinkleFeatureJson() {
+        AgeClassifier.mergeAgeToWrinkleFeaturesFromJsons("1");
+    }
+
+    @Test
+    public void clusterDataByFuzzyKMeans() {
+        //   AgeClassifier.clusterDataByFuzzyKMeans("dump\\mergedAgeToWrinkleFeatureJson1.txt");
+
+    }
+}
