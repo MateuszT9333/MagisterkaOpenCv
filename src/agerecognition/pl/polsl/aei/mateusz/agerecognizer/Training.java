68ec66b13df48bd24e3e7ee6f334764c137aa90c
Mateusz <t.mateusz9333@gmail.com>
2019-04-27 11:56:25 +0200

    Refaktor

---
+++ src/agerecognition/pl/polsl/aei/mateusz/agerecognizer/Training.java
@@ -0,0 +1,47 @@
+package pl.polsl.aei.mateusz.agerecognizer;
+
+import org.junit.Test;
+import pl.polsl.aei.mateusz.agerecognizer.train.AgeClassifier;
+import pl.polsl.aei.mateusz.agerecognizer.utils.PropertiesLoader;
+
+import java.io.File;
+
+public class Training {
+    private static final PropertiesLoader propertiesLoader = PropertiesLoader.getInstance();
+
+    @Test
+    public void generateDataFromImagesManual() {
+        AgeClassifier.generateDataFromImages("1", "41");
+    }
+
+    @Test
+    public void generateDataFromImagesAuto() throws InterruptedException {
+        boolean process = false;
+        File images = new File(propertiesLoader.getProperty("trainingImagesPath"));
+        String startFrom = "19";
+        for (File image : images.listFiles()) {
+
+            if (image.getName().contains(startFrom)) {
+                process = true;
+            }
+
+            if (process == false) {
+                continue;
+            }
+
+            AgeClassifier.generateDataFromImages("1", image.getName());
+            Thread.sleep(10000);
+        }
+    }
+
+    @Test
+    public void mergeAgeToWrinkleFeatureJson() {
+        AgeClassifier.mergeAgeToWrinkleFeaturesFromJsons("1");
+    }
+
+    @Test
+    public void clusterDataByFuzzyKMeans() {
+        //   AgeClassifier.clusterDataByFuzzyKMeans("dump\\mergedAgeToWrinkleFeatureJson1.txt");
+
+    }
+}
