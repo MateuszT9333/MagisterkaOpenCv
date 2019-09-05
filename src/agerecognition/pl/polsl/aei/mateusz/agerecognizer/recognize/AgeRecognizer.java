a9458fd50151e6258dd81482d4d8c51d1bd81ee0
Mateusz <t.mateusz9333@gmail.com>
2019-09-05 23:39:05 +0200

    metoda originalna i wlasna 10 grup

--- src/agerecognition/pl/polsl/aei/mateusz/agerecognizer/recognize/AgeRecognizer.java
+++ src/agerecognition/pl/polsl/aei/mateusz/agerecognizer/recognize/AgeRecognizer.java
@@ -11,11 +11,11 @@ import java.util.List;
 
 public class AgeRecognizer {
     private static final Logger log = LogManager.getLogger("main");
-    private static float[] wrinkleCentersOriginalMethod = {1, 2, 3, 4, 5};
-    private static float[] ageCentersOriginalMethod = {10, 20, 30, 40, 50};
+    private static float[] wrinkleCentersOriginalMethod = {0.2208f,0.6008f,1.2694f,0.4562f,1.0134f,1.8540f,0.3345f,0.7478f,1.4942f,0.1051f};
+    private static float[] ageCentersOriginalMethod = {30.491f,34.592f,63.723f,36.716f,52.652f,60.382f,32.755f,45.834f,58.551f,25.219f};
 
-    private static float[] wrinkleCentersCustomMethod = {1, 2, 3, 4, 5};
-    private static float[] ageCentersCustomMethod = {10, 20, 30, 40, 50};
+    private static float[] wrinkleCentersCustomMethod = {0.2663f,1.5699f,0.1723f,0.0776f,0.6121f,0.7963f,1.0737f,0.3641f,0.4720f,1.3270f};
+    private static float[] ageCentersCustomMethod = {26.389f,65.444f,26.366f,21.836f,34.086f,40.008f,54.692f,29.243f,30.444f,52f};
     private static boolean originalMethod = true;
 
     public static void recognizeAge(File imagePath) {
